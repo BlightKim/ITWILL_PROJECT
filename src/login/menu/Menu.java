b5f07b61b1d4180df3ad2e7a7f06bb441e65a8cc
ksb96 <ksebin96@gmail.com>
2023-02-23 14:13:17 +0900

    2022-02-23 12:12

--- src/login/menu/Menu.java
+++ src/login/menu/Menu.java
@@ -4,6 +4,9 @@ import dao.RegisterDao;
 import dao.RegisterDaoImpl;
 import register.Register;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Menu implements Runnable {
@@ -19,32 +22,39 @@ public class Menu implements Runnable {
 
   @Override
   public void run() {
+    String choice = "";
     int loginTry = 0; // 로그인 시도횟수
-    int choiceNum = 0;
+
     menu:
     while (true) {
 
-      System.out.println("1.로그인  2.회원가입  3.프로그램 종료");
+      showLogo(); // 로고 보여주기
 
-      System.out.print("번호를 선택해주세요. >>");
+      System.out.print("이용을 원하시면 엔터를 눌러주세요. (프로그램 종료는 0번)");
 
-      String choice = sc.nextLine();
+      if ((choice = sc.nextLine()).equals("")) {
+
+        System.out.println("1.로그인  2.회원가입  3.프로그램 종료");
+
+        System.out.print("번호를 선택해주세요. >>");
+
+        choice = sc.nextLine();
 
         switch (choice) {
           case "1":
-            login :
-            while(true) {
+            login:
+            while (true) {
               String id = ""; // 아이디 변수
               String password = ""; // 비밀번호 변수
 
               System.out.print("아이디를 입력하세요(뒤로가기 : 0) >>");
               id = sc.nextLine();
-              if("0".equals(id))
+              if ("0".equals(id))
                 continue menu;
 
               System.out.print("비밀번호를 입력하세요(뒤로가기 : 0) >>");
               password = sc.nextLine();
-              if("0".equals(password))
+              if ("0".equals(password))
                 continue menu;
 
               if (registerDao.login(id, password)) { // 로그인 성공
@@ -54,7 +64,7 @@ public class Menu implements Runnable {
                 System.out.println("아이디 비밀번호가 일치하지 않습니다.");
                 loginTry++; // 로그인 횟수 1회 증가
 
-                if(loginTry > 5) { // 만약 로그인 횟수가 5회 이상 초과하면
+                if (loginTry > 5) { // 만약 로그인 횟수가 5회 이상 초과하면
                   System.out.println("로그인 횟수가 5회를 초과하였습니다. 프로그램을 종료합니다.");
                   break menu;
 
@@ -64,17 +74,41 @@ public class Menu implements Runnable {
               }
             }
 
-          case "2":
+          case "2": // 2번 누르면 회원가입 실행
             register = new Register(this.sc);
             register.run();
             continue menu;
 
-          case "3": // 프로그램 종료
+          case "3": // 3번 누르면 프로그램 종료
             break menu;
         }
-
-
-
+      } else if("0".equals(choice)) { // 0번 누르면
+        break menu; // 프로그램 종료
+      }
     }
   }
+
+  private static int sInread(String id) {
+    int readValue = 0;
+    try {
+      readValue = System.in.read("0".getBytes());
+      return readValue;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void showLogo() {
+    System.out.println("\r\n"
+            + "______  _____  _      _____  _   _  _____ ______ __   __        _   _  _____ ______  _____ \r\n"
+            + "|  _  \\|  ___|| |    |_   _|| | | ||  ___|| ___ \\\\ \\ / /       | | | ||  ___|| ___ \\|  _  |\r\n"
+            + "| | | || |__  | |      | |  | | | || |__  | |_/ / \\ V /        | |_| || |__  | |_/ /| | | |\r\n"
+            + "| | | ||  __| | |      | |  | | | ||  __| |    /   \\ /         |  _  ||  __| |    / | | | |\r\n"
+            + "| |/ / | |___ | |____ _| |_ \\ \\_/ /| |___ | |\\ \\   | |         | | | || |___ | |\\ \\ \\ \\_/ /\r\n"
+            + "|___/  \\____/ \\_____/ \\___/  \\___/ \\____/ \\_| \\_|  \\_/         \\_| |_/\\____/ \\_| \\_| \\___/ \r\n"
+            + "                                                                                           \r\n"
+            + "                                                                                           \r\n"
+            + "");
+  }
 }
+
